b96416120e97914667eca51840dd4d364ca6a7fd
thaisrqueiroz <thaisrochadequeiroz@gmail.com>
2025-09-17 16:44:00 +0200

    feat(CustomUserDetail): add getPassword, getUsername and getUser

---
+++ src/main/java/pildonitas/pdnt/security/CustomUserDetail.java
@@ -0,0 +1,34 @@
+package pildonitas.pdnt.security;
+
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import pildonitas.pdnt.user.User;
+
+import java.util.Collection;
+import java.util.List;
+
+@AllArgsConstructor
+public class CustomUserDetail implements UserDetails {
+    private final User user;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    public User getUser() {
+        return user;
+    }
+}
