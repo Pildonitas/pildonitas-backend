b751cdf89387424afef92f7fddc16cb6ee7242ce
thaisrqueiroz <thaisrochadequeiroz@gmail.com>
2025-09-17 12:01:38 +0200

    initial commit

---
+++ src/test/java/pildonitas/pdnt/PdntApplicationTests.java
@@ -0,0 +1,13 @@
+package pildonitas.pdnt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdntApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
